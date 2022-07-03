9777d69c9a624fc38e6a6d1c60b836991e3cb050
Nidhiy Deshpande <78918220+Nidhiy-D@users.noreply.github.com>
2022-07-03 22:16:57 +0530

    Last Questions of Algorithm I

---
+++ Algorithm I/Day 14.cpp
@@ -0,0 +1,101 @@
+/*
+Question 1
+
+190. Reverse Bits
+
+Reverse bits of a given 32 bits unsigned integer.
+
+Note:
+
+Note that in some languages, such as Java, there is no unsigned integer type. In this case, both input and output will be given as a signed integer type. They should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above, the input represents the signed integer -3 and the output represents the signed integer -1073741825.
+ 
+
+Example 1:
+
+Input: n = 00000010100101000001111010011100
+Output:    964176192 (00111001011110000010100101000000)
+Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+
+Example 2:
+
+Input: n = 11111111111111111111111111111101
+Output:   3221225471 (10111111111111111111111111111111)
+Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10111111111111111111111111111111.
+ 
+
+Constraints:
+
+The input must be a binary string of length 32
+ 
+
+Follow up: If this function is called many times, how would you optimize it?
+
+*/
+
+//Solution
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        
+        n = ((n & 0xffff0000) >> 16) | ((n & 0x0000ffff) << 16);
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+        
+        return n;
+        
+    }
+};
+
+////////////////////////////////////////////////////////////////////
+
+/*
+Question 2
+136. Single Number
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example 1:
+
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+Output: 4
+Example 3:
+
+Input: nums = [1]
+Output: 1
+ 
+
+Constraints:
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+
+*/
+
+//Solution
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        sort(nums.begin(), nums.end());
+        for( int i =1; i< nums.size(); i+=2)
+        {
+            if(nums[i] != nums[i-1])
+                return nums[i-1];
+        }
+        return nums[nums.size()-1];
+    }
+};
+
+////////////////////////////////////////////////////////////////////
